407c7a5389f0759834623fd93028cfc27a21e225
奥田誠 <lantan9484@gmail.com>
2019-05-09 04:33:23 +0900

    3Dゲームアプリを作ろうVol2 課題&発展課題
    カリキュラムLesson6:3Dゲームアプリを作ろう Vol.2の提出課題と発展課題です。
    課題の、不要になったアイテムを順次破棄するスクリプトについては「ItemDestroyer」に記述、発展課題のアイテム動的生成のスクリプトについては「ItemGenerator」に記述してあります。

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+
+	private GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+		//unityちゃんオブジェクトを取得
+		this.unitychan = GameObject.Find("unitychan");
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		//unityちゃんとアイテムの距離を測定
+		float item_difference = unitychan.transform.position.z - this.transform.position.z;
+		//unityちゃんがアイテムを追い越した時、アイテムを破棄
+		if(item_difference >= 10){
+			Destroy (this.gameObject);
+	}
+}
+
+}
